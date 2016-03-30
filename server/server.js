c4a6e21ed7b60125109251cea95c1e686fcd307e
MartinN13 <martinnabhan@me.com>
2016-03-30 22:01:20 +0200

    Add search match score to /room/searchp/ route, add utf-8 encoding to all routes

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ require('fs').readFile('../salar.json', 'utf8', function (err, data) {
  * @param  Integer       code    HTTP status code
  */
 function sendJSONResponse(res, content, code = 200) {
-    res.writeHead(code, "Content-Type: application/json");
+    res.writeHead(code, { 'content-type': 'application/json; charset=utf-8' });
     res.write(JSON.stringify(content, null, "    "));
     res.end();
 
@@ -91,7 +91,7 @@ router.get("/", (req, res) => {
         + " /room/search/:search        Search for a room.\n"
     );
 
-    res.writeHead(200, "Content-Type: text/plain");
+    res.writeHead(200, { 'content-type': 'text/plain; charset=utf-8' });
     res.write(indexMessage);
     res.end();
 
@@ -185,61 +185,68 @@ router.get("/room/search/:search", (req, res) => {
     var search = String(req.params.search);
 
     var numberMatch = obj.salar.filter(function(sal) {
-        if (sal.Salsnr != null) {
+        if (sal.Salsnr !== null) {
             return sal.Salsnr.indexOf(search) !== -1;
         }
     });
 
     var nameMatch = obj.salar.filter(function(sal) {
-        if (sal.Salsnamn != null) {
+        if (sal.Salsnamn !== null) {
             return sal.Salsnamn.indexOf(search) !== -1;
         }
     });
 
     var latMatch = obj.salar.filter(function(sal) {
-        if (sal.Lat != null) {
+        if (sal.Lat !== null) {
             return sal.Lat.indexOf(search) !== -1;
         }
     });
 
     var longMatch = obj.salar.filter(function(sal) {
-        if (sal.Lot != null) {
-            return sal.Lot.indexOf(search) !== -1;
+        if (sal.Long !== null) {
+            return sal.Long.indexOf(search) !== -1;
         }
     });
 
     var placeMatch = obj.salar.filter(function(sal) {
-        if (sal.Ort != null) {
+        if (sal.Ort !== null) {
             return sal.Ort.indexOf(search) !== -1;
         }
     });
 
     var houseMatch = obj.salar.filter(function(sal) {
-        if (sal.Hus != null) {
+        if (sal.Hus !== null) {
             return sal.Hus.indexOf(search) !== -1;
         }
     });
 
     var floorMatch = obj.salar.filter(function(sal) {
-        if (sal.Våning != null) {
+        if (sal.Våning !== null) {
             return sal.Våning.indexOf(search) !== -1;
         }
     });
 
     var typeMatch = obj.salar.filter(function(sal) {
-        if (sal.Typ != null) {
+        if (sal.Typ !== null) {
             return sal.Typ.indexOf(search) !== -1;
         }
     });
 
     var sizeMatch = obj.salar.filter(function(sal) {
-        if (sal.Storlek != null) {
+        if (sal.Storlek !== null) {
             return sal.Storlek.indexOf(search) !== -1;
         }
     });
 
     var searchMatch = numberMatch.concat(houseMatch, nameMatch, latMatch, longMatch,
                             placeMatch, floorMatch, typeMatch, sizeMatch);
+
+    // ES6 function using Set() to delete duplicates
+    function uniq(a) {
+        return Array.from(new Set(a));
+    }
+
+    searchMatch = uniq(searchMatch);
 
     if (queryKey == "max") {
       var objFiltered = JSON.stringify(searchMatch, null, "    ").split("\n");
@@ -270,63 +277,64 @@ router.get("/room/search/:search", (req, res) => {
 router.get("/room/searchp/:search", (req, res) => {
     var search = String(req.params.search);
 
+    // Exact matches
     var numberMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Salsnr != null) {
+        if (sal.Salsnr !== null) {
             return sal.Salsnr == search;
         }
     });
 
     var nameMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Salsnamn != null) {
+        if (sal.Salsnamn !== null) {
             return sal.Salsnamn == search;
         }
     });
 
     var latMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Lat != null) {
+        if (sal.Lat !== null) {
             return sal.Lat == search;
         }
     });
 
     var longMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Lot != null) {
-            return sal.Lot == search;
+        if (sal.Long !== null) {
+            return sal.Long == search;
         }
     });
 
     var placeMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Ort != null) {
+        if (sal.Ort !== null) {
             return sal.Ort == search;
         }
     });
 
     var houseMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Hus != null) {
+        if (sal.Hus !== null) {
             return sal.Hus == search;
         }
     });
 
     var floorMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Våning != null) {
+        if (sal.Våning !== null) {
             return sal.Våning == search;
         }
     });
 
     var typeMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Typ != null) {
+        if (sal.Typ !== null) {
             return sal.Typ == search;
         }
     });
 
     var sizeMatchExact = obj.salar.filter(function(sal) {
-        if (sal.Storlek != null) {
+        if (sal.Storlek !== null) {
             return sal.Storlek == search;
         }
     });
 
-    function relevance(score, match) {
+    function relevanceExact(score, match, fieldTypeScore) {
       if (match.length > 0) {
-        score = score + 0.5;
+        score = score + 0.5 + fieldTypeScore;
       }
       return score;
     }
@@ -341,22 +349,182 @@ router.get("/room/searchp/:search", (req, res) => {
         typeMatchExactScore = 0.0,
         sizeMatchExactScore = 0.0;
 
-    numberMatchExactScore = relevance(numberMatchExactScore, numberMatchExact);
-    houseMatchExactScore = relevance(houseMatchExactScore, houseMatchExact);
-    nameMatchExactScore = relevance(nameMatchExactScore, nameMatchExact);
-    latMatchExactScore = relevance(latMatchExactScore, latMatchExact);
-    longMatchExactScore = relevance(longMatchExactScore, longMatchExact);
-    placeMatchExactScore = relevance(placeMatchExactScore, placeMatchExact);
-    floorMatchExactScore = relevance(floorMatchExactScore, floorMatchExact);
-    typeMatchExactScore = relevance(typeMatchExactScore, typeMatchExact);
-    sizeMatchExactScore = relevance(sizeMatchExactScore, sizeMatchExact);
+    numberMatchExactScore = relevanceExact(numberMatchExactScore, numberMatchExact, 0.5);
+    houseMatchExactScore = relevanceExact(houseMatchExactScore, houseMatchExact, 0.4);
+    nameMatchExactScore = relevanceExact(nameMatchExactScore, nameMatchExact, 0.4);
+    latMatchExactScore = relevanceExact(latMatchExactScore, latMatchExact, 0.1);
+    longMatchExactScore = relevanceExact(longMatchExactScore, longMatchExact, 0.1);
+    placeMatchExactScore = relevanceExact(placeMatchExactScore, placeMatchExact, 0.3);
+    floorMatchExactScore = relevanceExact(floorMatchExactScore, floorMatchExact, 0.3);
+    typeMatchExactScore = relevanceExact(typeMatchExactScore, typeMatchExact, 0.3);
+    sizeMatchExactScore = relevanceExact(sizeMatchExactScore, sizeMatchExact, 0.2);
 
-    // Next add 0.3(?) if something partially matches
+    for (var element in numberMatchExact) {
+        numberMatchExact[element].Poäng = numberMatchExactScore;
+    }
 
-    // How do I return the score with each match? Maybe add a json item named score?
+    for (element in houseMatchExact) {
+        houseMatchExact[element].Poäng = houseMatchExactScore;
+    }
 
-    var searchMatch = numberMatchExact.concat(houseMatchExact, nameMatchExact, latMatchExact, longMatchExact,
-                            placeMatchExact, floorMatchExact, typeMatchExact, sizeMatchExact);
+    for (element in nameMatchExact) {
+        nameMatchExact[element].Poäng = nameMatchExactScore;
+    }
+
+    for (element in latMatchExact) {
+        latMatchExact[element].Poäng = latMatchExactScore;
+    }
+
+    for (element in longMatchExact) {
+        longMatchExact[element].Poäng = longMatchExactScore;
+    }
+
+    for (element in placeMatchExact) {
+        placeMatchExact[element].Poäng = placeMatchSExactcore;
+    }
+
+    for (element in floorMatchExact) {
+        floorMatchExact[element].Poäng = floorMatchExactScore;
+    }
+
+    for (element in typeMatchExact) {
+        typeMatchExact[element].Poäng = typeMatchExactScore;
+    }
+
+    for (element in sizeMatchExact) {
+        sizeMatchExact[element].Poäng = sizeMatchExactScore;
+    }
+
+    // Partial matches
+    var numberMatch = obj.salar.filter(function(sal) {
+        if (sal.Salsnr !== null && sal.Salsnr != search) {
+            return sal.Salsnr.indexOf(search) !== -1;
+        }
+    });
+
+    var nameMatch = obj.salar.filter(function(sal) {
+        if (sal.Salsnamn !== null && sal.Salsnamn != search) {
+            return sal.Salsnamn.indexOf(search) !== -1;
+        }
+    });
+
+    var latMatch = obj.salar.filter(function(sal) {
+        if (sal.Lat !== null && sal.Lat != search) {
+            return sal.Lat.indexOf(search) !== -1;
+        }
+    });
+
+    var longMatch = obj.salar.filter(function(sal) {
+        if (sal.Long !== null && sal.Long != search) {
+            return sal.Long.indexOf(search) !== -1;
+        }
+    });
+
+    var placeMatch = obj.salar.filter(function(sal) {
+        if (sal.Ort !== null && sal.Ort != search) {
+            return sal.Ort.indexOf(search) !== -1;
+        }
+    });
+
+    var houseMatch = obj.salar.filter(function(sal) {
+        if (sal.Hus !== null && sal.Hus != search) {
+            return sal.Hus.indexOf(search) !== -1;
+        }
+    });
+
+    var floorMatch = obj.salar.filter(function(sal) {
+        if (sal.Våning !== null && sal.Våning != search) {
+            return sal.Våning.indexOf(search) !== -1;
+        }
+    });
+
+    var typeMatch = obj.salar.filter(function(sal) {
+        if (sal.Typ !== null && sal.Typ != search) {
+            return sal.Typ.indexOf(search) !== -1;
+        }
+    });
+
+    var sizeMatch = obj.salar.filter(function(sal) {
+        if (sal.Storlek !== null && sal.Storlek != search) {
+            return sal.Storlek.indexOf(search) !== -1;
+        }
+    });
+
+    function relevance(score, match, fieldTypeScore) {
+      if (match.length > 0) {
+        score = score + 0.3 + fieldTypeScore;
+      }
+      return score;
+    }
+
+    var numberMatchScore = 0.0,
+    houseMatchScore = 0.0,
+    nameMatchScore = 0.0,
+    latMatchScore = 0.0,
+    longMatchScore = 0.0,
+    placeMatchScore = 0.0,
+    floorMatchScore = 0.0,
+    typeMatchScore = 0.0,
+    sizeMatchScore = 0.0;
+
+    numberMatchScore = relevance(numberMatchScore, numberMatch, 0.5);
+    houseMatchScore = relevance(houseMatchScore, houseMatch, 0.4);
+    nameMatchScore = relevance(nameMatchScore, nameMatch, 0.4);
+    latMatchScore = relevance(latMatchScore, latMatch, 0.1);
+    longMatchScore = relevance(longMatchScore, longMatch, 0.1);
+    placeMatchScore = relevance(placeMatchScore, placeMatch, 0.3);
+    floorMatchScore = relevance(floorMatchScore, floorMatch, 0.3);
+    typeMatchScore = relevance(typeMatchScore, typeMatch, 0.3);
+    sizeMatchScore = relevance(sizeMatchScore, sizeMatch, 0.2);
+
+    for (element in numberMatch) {
+        numberMatch[element].Poäng = numberMatchScore;
+    }
+
+    for (element in houseMatch) {
+        houseMatch[element].Poäng = houseMatchScore;
+    }
+
+    for (element in nameMatch) {
+        nameMatch[element].Poäng = nameMatchScore;
+    }
+
+    for (element in latMatch) {
+        latMatch[element].Poäng = latMatchScore;
+    }
+
+    for (element in longMatch) {
+        longMatch[element].Poäng = longMatchScore;
+    }
+
+    for (element in placeMatch) {
+        placeMatch[element].Poäng = placeMatchScore;
+    }
+
+    for (element in floorMatch) {
+        floorMatch[element].Poäng = floorMatchScore;
+    }
+
+    for (element in typeMatch) {
+        typeMatch[element].Poäng = typeMatchScore;
+    }
+
+    for (element in sizeMatch) {
+        sizeMatch[element].Poäng = sizeMatchScore;
+    }
+
+    // Concat is ordered by priority of each field
+    var searchMatch = numberMatch.concat(numberMatchExact, nameMatchExact, nameMatch, houseMatchExact, houseMatch,
+                                              placeMatchExact, placeMatch, floorMatchExact, floorMatch,
+                                              typeMatchExact, typeMatch, sizeMatchExact, sizeMatch,
+                                              latMatchExact, latMatch, longMatchExact, longMatch);
+
+    // ES6 function using Set() to delete duplicates
+    function uniq(a) {
+        return Array.from(new Set(a));
+    }
+
+    searchMatch = uniq(searchMatch);
 
     if (queryKey == "max") {
       var objFiltered = JSON.stringify(searchMatch, null, "    ").split("\n");
